7c13dc70f7fe9103218ebe5db82d7bf3854d5c2c
elmandoloriano333 <raikague123@outlook.es>
2020-03-09 10:49:32 +0100

    examen

---
+++ Eventos1/Eventos1/Empleado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos1
+{
+
+    public delegate void ActualizacionEventHandler(string msg);
+
+    public class multiplo
+        {
+        //atributo
+        private int numero;
+
+            //declaración del evento
+            public event ActualizacionEventHandler NombreCambiado;
+
+            public multiplo(int numero)
+            {
+                this.numero = numero;
+            }
+
+            public void setnumero(int  s)
+            {
+                //actualizar el atributo
+               int numeroera = this.numero;
+                this.numero = s;
+
+            //lanzar el evento
+             n = 4;
+            if  (s/n)
+             if (this.multiploes != null)
+                    this.multiploes("el numero Era " + numeroera + " el resultado de su multiplo es "+this.numero);
+            }
+
+        }
+   }
+
+
---
+++ Eventos1/Eventos1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Eventos1
+{
+    class Program
+    {
+        public static void Notificarmultiplo(int msg)
+        {
+            //imprimos mensaje aviso.
+            Console.WriteLine("Es multiplo de 4!");
+            Console.WriteLine(msg);
+        }
+
+        public static void Main(string[] args)
+        {
+           multiplo Mimultiplo = new multiplo("Ramiro");
+            Mimultiplo.multiploes +=Notificarmultiploes; //suscribiendo
+            Console.Write("el numero es ");
+            MiEmpleado.setNombre(Console.ReadLine());
+
+            Console.Write("Pulsa cualquier tecla para continuar . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
