ef6650205894cd33474d2892b81dcf6cedd25a6c
elmandaloriano333 <raikague123@outlook.es>
2020-03-24 02:30:27 -0400

    tarea fibonnacci

---
+++ FibonacciRecursivo/FibonacciRecursivo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FibonacciRecursivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciRecursivo f = new FibonacciRecursivo();
+
+
+            
+            for (int num=0; num <=9; num++)
+            {
+                
+                Console.WriteLine(f.fibonacci(num));
+            }
+            Console.ReadKey();
+        }
+    }
+}
