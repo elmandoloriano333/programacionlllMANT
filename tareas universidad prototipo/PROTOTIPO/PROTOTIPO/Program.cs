19ef689dc2e59504162ffd7e61938b1e4dc5dc89
elmandaloriano333 <raikague123@outlook.es>
2020-04-26 23:58:18 -0400

    practica

---
+++ tareas universidad prototipo/PROTOTIPO/PROTOTIPO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROTOTIPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celular a = new Celular(Marca.LG, ":)", new TamañoPantalla(10, 6), Color.Negro, 16 , new Procesador(" :( ", 7, 3 ));
+            Celular b = (Celular)a.Clone();
+            Celular b1 = (Celular)a.Clone();
+            Celular b2= (Celular)a.Clone();
+            Celular b3 = (Celular)a.Clone();
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tareas universidad prototipo/PROTOTIPO/PROTOTIPO/TamañoPantalla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROTOTIPO
+{
+    class TamañoPantalla
+    {
+        int alto;
+        int ancho;
+
+        public TamañoPantalla(int alto, int ancho)
+        {
+            this.alto = alto;
+            this.ancho = ancho;
+        }
+
+        public override string ToString()
+        {
+            return alto.ToString() + ancho.ToString();
+        }
+    }
+}
